d84ce5e0b5e09bd04306f9b9fe59ce64bfe6a723
GMATS <gerardo.matsui@gmail.com>
2016-09-09 17:28:16 -0300

    Agrego el inicio del dashboard

--- application/safe_api/scripts/safe_db.sql
+++ application/safe_api/scripts/safe_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 24-08-2016 a las 21:39:45
+-- Tiempo de generación: 09-09-2016 a las 22:05:39
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 5.6.21
 
@@ -32,6 +32,17 @@ CREATE TABLE `alumno` (
   `usuario_id` int(11) NOT NULL,
   `instituto_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`id`, `legajo`, `usuario_id`, `instituto_id`) VALUES
+(1, '123457', 2, 1),
+(2, '123452', 3, 1),
+(3, '123422', 4, 1),
+(4, '123423', 5, 1),
+(5, '123424', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -112,12 +123,16 @@ CREATE TABLE `curso` (
   `titulo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `descripcion` longtext COLLATE utf8mb4_unicode_ci,
   `fechaCreacion` datetime DEFAULT NULL,
-  `instituto_id` int(11) DEFAULT NULL
+  `instituto_id` int(11) DEFAULT NULL,
+  `fechaModificacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `curso`
 --
+
+INSERT INTO `curso` (`id`, `titulo`, `descripcion`, `fechaCreacion`, `instituto_id`, `fechaModificacion`) VALUES
+(4, 'Matemáticas 1', '<h1>Curso inical de matemáticas</h1> <p>El objetivo del curso...</p>', '2016-08-25 00:43:30', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -130,6 +145,14 @@ CREATE TABLE `curso_alumno` (
   `alumno_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `curso_alumno`
+--
+
+INSERT INTO `curso_alumno` (`curso_id`, `alumno_id`) VALUES
+(4, 3),
+(4, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -140,6 +163,14 @@ CREATE TABLE `curso_docente` (
   `curso_id` int(11) NOT NULL,
   `docente_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `curso_docente`
+--
+
+INSERT INTO `curso_docente` (`curso_id`, `docente_id`) VALUES
+(4, 1),
+(4, 2);
 
 -- --------------------------------------------------------
 
@@ -153,8 +184,19 @@ CREATE TABLE `docente` (
   `fechaModificacion` datetime DEFAULT NULL,
   `usuario_id` int(11) NOT NULL,
   `instituto_id` int(11) DEFAULT NULL,
-  `legajo` varchar(100) DEFAULT NULL
+  `legajo` varchar(100) DEFAULT NULL,
+  `fechaCreacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `docente`
+--
+
+INSERT INTO `docente` (`id`, `curriculum`, `fechaModificacion`, `usuario_id`, `instituto_id`, `legajo`, `fechaCreacion`) VALUES
+(1, '<h3>Educaci&oacute;n<h3> <ul><li>Instituto 1</li><li>Instituto 2</li></ul>', '2016-08-25 00:39:32', 7, 1, '123457', NULL),
+(2, '<h3>Educaci&oacute;n<h3> <ul><li>Instituto 1</li><li>Instituto 2</li></ul>', '2016-08-25 00:40:40', 8, 1, '123451', NULL),
+(3, '<h3>Educaci&oacute;n<h3> <ul><li>Instituto 1</li><li>Instituto 2</li></ul>', '2016-08-25 00:41:40', 9, 1, '123452', NULL),
+(4, '<h3>Educaci&oacute;n<h3> <ul><li>Instituto 1</li><li>Instituto 2</li></ul>', '2016-08-25 00:41:54', 10, 1, '123454', NULL);
 
 -- --------------------------------------------------------
 
@@ -321,7 +363,16 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`, `nombre`, `apellido`, `avatar`, `tipo_documento_id`, `numero_documento`, `genero`, `nacionalidad`) VALUES
-(1, 'admin', 'admin', 'admin@safe.com', 'admin@safe.com', 1, '6lsk4s3xzukgos4wc8kkooc4g0gg0kw', 'fAz1MnIWmBmMm+MttEEuq6oABXfyFXsvMup1TMA6EokWvk7ARq3plY6gZDK6x25vVDmiWUkkSZEysf7sf8AdDA==', '2016-08-23 23:56:41', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL, 'admin_nombre', 'admin_apellido', NULL, 1, '20555777', NULL, NULL);
+(1, 'admin', 'admin', 'admin@safe.com', 'admin@safe.com', 1, '6lsk4s3xzukgos4wc8kkooc4g0gg0kw', 'fAz1MnIWmBmMm+MttEEuq6oABXfyFXsvMup1TMA6EokWvk7ARq3plY6gZDK6x25vVDmiWUkkSZEysf7sf8AdDA==', '2016-09-06 03:16:46', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL, 'admin_nombre', 'admin_apellido', NULL, 1, '20555777', NULL, NULL),
+(2, 'chespirito', 'chespirito', 'chespirito@organizacion.org', 'chespirito@organizacion.org', 1, 'pll1f8ota5cgcs0s0koog0gkgsc0s0', 'TGo5inENJH2gE3Qf3YxVFoky0OwPYD86inxtaD+bMoYJzCtNkZ4nPUdHWkiRzIO/5G8lfJSSeAtB0tZYShGeuQ==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:11:"ROLE_ALUMNO";}', 0, NULL, 'Roberto', 'Gómez Bolaño', NULL, 1, '30777555', 'Masculino', NULL),
+(3, 'alumno1', 'alumno1', 'alumno1@organizacion.org', 'alumno1@organizacion.org', 1, 'm009bk38wtso4wkssg4sc8g404wok4w', 'FkaApz8FAz5iZrESTHBKVLJy8zV8UBQcmJqdDrEXMMuI1kiJgrfrJ9ZXgQPtV/i9V3j1RtvlszMyqC4ahInhlw==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:11:"ROLE_ALUMNO";}', 0, NULL, 'Alumno_1', 'Gómez Bolaño', NULL, 1, '30777551', 'Masculino', NULL),
+(4, 'alumno2', 'alumno2', 'alumno2@organizacion.org', 'alumno2@organizacion.org', 1, 'p5lnsdli5344cwgsgssc0c4sc04o4w0', 'F/TIIq7DfD6M1Hk6uLKuS+VmkrWPkAyAkmzZ7blhpfJgjUeG81Mt3oRoCI2r3pftLBzIVekdfGAltIuFBcX8bg==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:11:"ROLE_ALUMNO";}', 0, NULL, 'Alumno_2', 'Gómez Bolaño', NULL, 1, '30777552', 'Masculino', NULL),
+(5, 'alumno3', 'alumno3', 'alumno3@organizacion.org', 'alumno3@organizacion.org', 1, 'kialvu6njsgsco8wck4s0ws44wc4w0o', '/DxuzoEyMAb/KjMFiMxvafn6/NW0YlBPjlipqM31S6c+f3MkXU1l7yphBssAgwUqnB5/z9U7cOb8UMX1ISFdwA==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:11:"ROLE_ALUMNO";}', 0, NULL, 'Alumno_3', 'Gómez Bolaño', NULL, 1, '30777553', 'Masculino', NULL),
+(6, 'alumno4', 'alumno4', 'alumno4@organizacion.org', 'alumno4@organizacion.org', 1, 'joqg0c4jqeos0k4cs0cwgscwwckk8go', 'dcmdtls7c5JGcy3gpmfwUBewxfKDeg9AXcZRBfLCICR74gEid6N5+kSbwwKKeZmjLeRm9nT8BE5ReerxVcMX4Q==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:11:"ROLE_ALUMNO";}', 0, NULL, 'Alumno_4', 'Gómez Bolaño', NULL, 1, '30777554', 'Masculino', NULL),
+(7, 'jirafales', 'jirafales', 'jirafales@organizacion.org', 'jirafales@organizacion.org', 1, 'b48n6m4nz1ckkwogg800444w0c4wok8', 'E+XHNQ1Whx352vq24UBhrtOqW6/9flDGEbEYYD+pJ+jxdQAN2DPxzh0jpZo2J1oJbukDTAXvJ443sK0XtuDvvQ==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:12:"ROLE_DOCENTE";}', 0, NULL, 'Ruben', 'Aguirre', NULL, 1, '20777555', 'Masculino', NULL),
+(8, 'docente1', 'docente1', 'docente1@organizacion.org', 'docente1@organizacion.org', 1, 'xg4udz23ztw0kw4oco48gs008gg4g', 'RlNu8+03kQTcjs4hXE4OEqGUn7lONjSmFthvbbuGLOUVqoQXStVwnZ4MKp1ah8mDKat5QiQ7d4cLmzUlmlUKtw==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:12:"ROLE_DOCENTE";}', 0, NULL, 'docente1', 'Aguirre', NULL, 1, '20777551', 'Masculino', NULL),
+(9, 'docente2', 'docente2', 'docente2@organizacion.org', 'docente2@organizacion.org', 1, '4acpspaebzwgsc80s08ocswk848ssws', 'YdMcpTwOCIsjxCixlF2tv3pdHt7TrVl9i0dz/Yt5LxJIw5A2iltk6pV6nN1nOSbflyArnYOCRqd/239VBbcSeA==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:12:"ROLE_DOCENTE";}', 0, NULL, 'docente2', 'Aguirre', NULL, 1, '20777552', 'Masculino', NULL),
+(10, 'docente4', 'docente4', 'docente4@organizacion.org', 'docente4@organizacion.org', 1, 'kwipnamuxlc80so4sckokw0888g4ook', 'MmIYLc3UZwJB+UAG9/TkZaAyhJs1LeO/XmyZuvryN8PuzVtMcrpH0E0nHhxZOmaY4jQsVUehFvYk+kPzQdaRFw==', NULL, 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:12:"ROLE_DOCENTE";}', 0, NULL, 'docente4', 'Aguirre', NULL, 1, '20777554', 'Masculino', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -470,7 +521,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `classification__category`
 --
@@ -490,12 +541,12 @@ ALTER TABLE `classification__tag`
 -- AUTO_INCREMENT de la tabla `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `docente`
 --
 ALTER TABLE `docente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `instituto`
 --
@@ -530,7 +581,7 @@ ALTER TABLE `tipo_documento`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Restricciones para tablas volcadas
 --
