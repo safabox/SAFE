979b2bcd2b71a531c7c06329c8cba1b2c5fb8b49
safaboxc <safaboxc@safabox.com>
2016-10-20 14:40:01 -0300

    Agrego descripcion

--- application/safe_api/src/Safe/CursoBundle/Entity/Curso.php
+++ application/safe_api/src/Safe/CursoBundle/Entity/Curso.php
@@ -52,7 +52,6 @@ class Curso
      *
      * @ORM\Column(name="descripcion", type="text", nullable=true)     
      * @Expose
-     * @Groups({"admin_detalle"}) 
      */
     private $descripcion;
 
