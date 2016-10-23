9cee6874875f4747d0b774150706cb69afb3e593
GMATS <gerardo.matsui@gmail.com>
2016-10-23 19:47:30 -0300

    Prototipo de la visualizacion del alumno para las actividades

--- application/safe_ui/client/app/alumnos/actividades/alumno.actividades.home.controller.js
+++ application/safe_ui/client/app/alumnos/actividades/alumno.actividades.home.controller.js
@@ -2,12 +2,13 @@
     'use strict';
 
     angular.module('app.alumno')
-        .controller('AlumnoActividadHomeCtrl', ['$scope', '$state', '$q', AlumnoActividadHomeCtrl])
+        .controller('AlumnoActividadHomeCtrl', ['$scope', '$state', '$uibModal','$q', AlumnoActividadHomeCtrl])
 
-    function AlumnoActividadHomeCtrl($scope, $state, $q) {
+    function AlumnoActividadHomeCtrl($scope, $state, $uibModal, $q) {
         var vm = this;
         vm.loading = true;
         vm.goBack = goBack;
+        vm.viewConcept = viewConcept;
         $q.all([])
             .then(onLoadComplete);
         
@@ -16,6 +17,18 @@
         }
         function goBack() {
              $state.go('alumno.concepto.dashboard');
+        }
+        function viewConcept() {
+            var modalInstance = $uibModal.open({
+                templateUrl: 'concepto.html',
+                size: 'lg'                
+              });
+
+            modalInstance.result.then(function () {
+              concole.log("ok");
+            }, function () {
+              console.log("error");
+            });
         }
       
     }
